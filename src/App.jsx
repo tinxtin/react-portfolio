bb5c95435ffd92a2879dae8181ebfae97115b239
Thinh <thinh1198@hotmail.com>
2024-03-27 22:05:37 +0000

    add layout

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
 
 
     return (
-        <div id='bgGround' className='h-dvh w-screen bg-neutral-950 text-gray-300'>
+        <div id='bgGround' className='h-dvh w-screen flex justify-between px-24 gap-4 bg-neutral-950 text-gray-300'>
             <div id='bgEffect' className='h-full w-full relative'>
                 <div id='bgHorizontalLine' className='h-full w-full fixed bg-gradient-to-r from-bgEffectColor from-1px to-transparent to-1px bg-[length:95px_95px] z-0'></div>
                 <div id='bgVerticalLine' className='h-full w-full fixed bg-gradient-to-b from-bgEffectColor from-1px to-transparent to-1px bg-[length:10px_95px] z-0'></div>
