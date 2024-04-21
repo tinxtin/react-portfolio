d33dc7c6e2145bf0eedbb597796f75606a12c6e7
Thinh <thinh1198@hotmail.com>
2024-04-21 20:40:48 +0100

    redone code to dry code

--- src/pages/contacts.jsx
+++ src/pages/contacts.jsx
@@ -3,8 +3,8 @@ function Contacts() {
 
     return (
         <section className='page w-full min-h-screen'>
-            <div className='absolute right-2space bottom-2space whitespace-nowrap text-wrap w-1/4 leading-normal'>
-                email.com
+            <div className='absolute right-2space bottom-2space whitespace-nowrap text-wrap w-1/4 leading-normal w-min'>
+                <span className=''>email@com</span>
             </div>
         </section>
     )
--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -1,52 +1,46 @@
-import Project from '../components/projects/project';
+import Cards from '../components/projects/project';
 import { useState, useEffect } from 'react'
 
 function Projects() {
 
-    const projects = {
-        1: {
+    const projectList = [
+        {
             name: 'Placeholder 1',
             date: 'Jan.2024',
             product: 'Portfolio',
             description: 'test',
+            tech: [
+                'JavaScript',
+                'Tailwind',
+                'React'
+            ]
         },
-        2: {
+        {
             name: 'Placeholder 2',
             date: 'Feb.2024',
             product: 'Portfolio',
             description: 'test',
+            tech: [
+                'NextJS',
+                'Tailwind',
+            ]
         },
-        3: {
-            name: 'Placeholder 3',
-            date: 'March.2024',
-            product: 'Portfolio',
-            description: 'test',
-        },
-        4: {
-            name: 'Placeholder 4',
-            date: 'April.2024',
-            product: 'Portfolio',
-            description: 'test',
-        },
-        5: {
-            name: 'Placeholder 5',
-            date: 'May.2024',
-            product: 'Portfolio',
-            description: 'test',
-        },
-    }
+    ]
+
+    let projects = projectList.map((project ,i) => {
+        return (
+            <li key={i}>
+                <Cards name={project.name} date={project.date} type={project.product} description={project.description} technology={project.tech}/>
+            </li>
+        )
+    })
 
     return (
         <section className='w-full min-h-screen page'>
             <div className='py-2space px-2space whitespace-nowrap'>
                 <div className="m-0 p-0">
                     <ul className='flex flex-col items-end gap-y-12'>
-                        <li>
-                            <Project name={projects[1].name} date={projects[1].date} type={projects[1].product} description={projects[1].description}/>
-                        </li>
-                        <li>
-                            <Project name={projects[2].name} date={projects[2].date} type={projects[2].product} description={projects[2].description}/>
-                        </li>
+                        {projects}
                     </ul>
                 </div>
             </div>
